706b958fe55fedc03d75a0b6867c3881cca8d3fc
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-08 13:20:44 +0300

    Add models to admin panel

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task.models import TaskType, Position, Worker, Team, Project, Task
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
+    list_filter = ("assignees", "task_type", "priority")
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
