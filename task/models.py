b427c20c7e5b75d9aaf6943f40d0bead96fb45e1
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-08 12:54:21 +0300

    Add models in models.py

--- task/models.py
+++ task/models.py
@@ -1,3 +1,92 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+PRIORITY_CHOICES = [
+    ("URGENT", "Urgent"),
+    ("HIGH", "High"),
+    ("MEDIUM", "Medium"),
+    ("LOW", "Low"),
+]
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    workers = models.ManyToManyField(Worker, related_name="teams")
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+        related_name="projects",
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+
+    priority = models.CharField(
+        max_length=9,
+        choices=PRIORITY_CHOICES,
+        default="Low",
+    )
+
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+
+    assignees = models.ManyToManyField(Worker, related_name="assigned_tasks")
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+
+    def __str__(self):
+        return f"{self.name} (deadline: {self.deadline})"
