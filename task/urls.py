4eeb92733084c23338855c6b1d2019cb2f33baab
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-08 16:06:38 +0300

    Add URLs, views and templates for Worker model

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,37 @@
 from django.urls import path
 
-from task.views import index
+from task.views import (
+    index,
+    WorkerListView,
+    WorkerCreateView,
+    WorkerUpdateView,
+    WorkerDeleteView,
+    WorkerDetailView,
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path(
+        "workers/<int:pk>/",
+        WorkerDetailView.as_view(),
+        name="worker-detail",
+    ),
+    path(
+        "workers/create/",
+        WorkerCreateView.as_view(),
+        name="worker-create"
+    ),
+    path(
+        "workers/<int:pk>/update/",
+        WorkerUpdateView.as_view(),
+        name="worker-update",
+    ),
+    path(
+        "workers/<int:pk>/delete/",
+        WorkerDeleteView.as_view(),
+        name="worker-delete"
+    ),
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,5 +1,50 @@
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-def index(request: HttpRequest):
-    return render(request, "task/index.html")
+from task.forms import WorkerForm
+from task.models import Worker, Project, Team
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    context = {
+        "num_workers": Worker.objects.count(),
+        "num_projects": Project.objects.count(),
+        "num_teams": Team.objects.count(),
+    }
+
+    return render(request, "task/index.html", context=context)
+
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    model = Worker
+    paginate_by = 5
+
+
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Worker
+    queryset = (Worker.objects.all()
+                .select_related("position")
+                .prefetch_related("teams__projects")
+                .order_by("username"))
+
+
+class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Worker
+    fields = ("first_name", "last_name", "email", "position")
+    success_url = reverse_lazy("task:worker-list")
+
+
+class WorkerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Worker
+    success_url = reverse_lazy("task:worker-list")
+
+
+class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Worker
+    form_class = WorkerForm
