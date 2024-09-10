1f7d506a7f5e78c675107943d818cd8641abd18a
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-10 16:19:50 +0300

    Fix signal for project removal: update worker tasks and associations

--- task/signals.py
+++ task/signals.py
@@ -4,25 +4,24 @@ from task.models import Team, Worker, Project, Task
 
 
 @receiver(m2m_changed, sender=Team.projects.through)
-def update_worker_projects_on_team_project_change(sender, instance, action, pk_set, **kwargs):
+def handle_project_removal_from_team(sender, instance, action, pk_set, **kwargs):
     if action == "post_remove":
         removed_projects = set(pk_set)
         for project_id in removed_projects:
-            project = Project.objects.get(id=project_id)
+            try:
+                project = Project.objects.get(id=project_id)
+            except Project.DoesNotExist:
+                continue
+
+            workers_to_update = Worker.objects.filter(projects=project)
+            for worker in workers_to_update:
+                worker.projects.remove(project)
+
+            tasks_to_update = Task.objects.filter(project=project)
+            for task in tasks_to_update:
+                for worker in task.assignees.all():
+                    if worker in workers_to_update:
+                        task.assignees.remove(worker)
+
             if not project.teams.exists():
-                for worker in project.workers.all():
-                    worker.projects.remove(project)
-
-
-
-@receiver(m2m_changed, sender=Project.workers.through)
-def remove_worker_from_tasks_on_project(sender, instance,
-                                        action, pk_set, **kwargs):
-
-    if action == "post_remove":
-        workers_removed = Worker.objects.filter(pk__in=pk_set)
-
-        for worker in workers_removed:
-            tasks = Task.objects.filter(project=instance, assignees=worker)
-            for task in tasks:
-                task.assignees.remove(worker)
+                project.delete()
