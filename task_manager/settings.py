4fcd5e75a353a717a84e874a1f8cfd8e04270d6c
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-15 22:17:46 +0300

    Implement context processor for ASSETS_ROOT setting

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "task.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -128,6 +129,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
