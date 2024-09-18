9a36e210b92284ef0b2e634731d75f932bafe211
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-18 17:59:04 +0300

    Add it-company-task-manager-scrm.onrender.com to ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "it-company-task-manager-scrm.onrender.com",
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
