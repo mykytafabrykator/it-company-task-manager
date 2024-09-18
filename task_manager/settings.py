3001b364a3bcbbc49ac872c21ae83e50230cf00a
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-18 16:58:51 +0300

    Update settings with DEBUG value

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
